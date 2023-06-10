d8f61fca0ca91312b8965ad7f08c1765ee501ff6
ilkerteymur <ilkerteymuroglu@gmail.com>
2023-06-10 18:41:44 +0300

    first mvp

---
+++ main.js
@@ -0,0 +1,32 @@
+// butona tıklandığı anda navigation alanına
+// toggle isminde class ekler
+// tekrar tıklanırsa classs ı çıkarır
+
+const btn = document.querySelector(".nav-btn");
+const nav = document.querySelector("nav");
+
+btn. addEventListener("click", () => {
+    nav.classList.toggle("toggle");
+});
+
+// videoları ekrana geldiği anda oynatma
+
+document.addEventListener("DOMContentLoaded", function () {
+    const videos = document.querySelectorAll("video");
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.play();
+            } else {
+                entry.target.pause();
+            }
+        });
+    });
+    videos.forEach((video) => {
+        observer.observe(video);
+    });
+});
+
+
+
+
